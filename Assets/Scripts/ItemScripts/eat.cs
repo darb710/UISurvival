0ddf6aaf5733f72afd64e0047f9a495903cbc05f
darb710 <mdd828@my.utsa.edu>
2017-04-24 16:33:53 -0500

    Added vital labels

--- Assets/Scripts/ItemScripts/eat.cs
+++ Assets/Scripts/ItemScripts/eat.cs
@@ -5,15 +5,21 @@ using System.Collections;
 public class eat : MonoBehaviour
 {
 
+    //HungerSlider vitals : PlayerVitals;
     public PlayerVitals vitals;
+    
+
     public void eatMe()
     {
+        
         if (System.Int32.Parse(this.transform.Find("Text").GetComponent<Text>().text) > 1)
         {
+            GameObject hunger = GameObject.FindWithTag("hunger");
             int tcount = System.Int32.Parse(this.transform.Find("Text").GetComponent<Text>().text) - 1;
             this.transform.Find("Text").GetComponent<Text>().text = "" + tcount;
-            
-         
+            hunger.GetComponent<PlayerVitals>().hungerSlider.value += 20;
+
+
         }
         else
         {
@@ -25,12 +31,25 @@ public class eat : MonoBehaviour
     }
     void Update()
     {
+       
         if (Input.GetKeyDown("1"))
         {
  
             eatMe();
-            vitals.eatFood(gameObject);
-
+            
+            //vitals.hungerSlider.value = vitals.hungerSlider.value + 20;
+            
+            //vitals.eatFood(gameObject);
+            //vitals.thirstSlider.value += 20;
+            // Debug.Log("Trying Slider", vitals);
         }
     }
+    void start()
+    {
+     
+        //vitals = Player.Find("PlayerVitals").GetComponent<PlayerVitals>().healthSlider.value;
+       // vitals = GameObject.Find("hungerSlider").GetComponent<PlayerVitals>();
+       // vitals = PlayerVitals.hungerSlider.transform.Find("hungerSlider").GetComponent<PlayerVitals>();
+        //vitals = GameObject.Find("hunderSlider").
+}
 }
